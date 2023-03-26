8a7b6d9afacd68984c1b83886d162bb2db805143
Usman Sharafa <Usmansharafa04@gmail.com>
2023-03-26 17:09:39 +0100

    Task 4 solved

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # Configure ssh config file
-file_line { 'ssh_config':
-  path => '/etc/ssh/ssh_config',
-  line => 'IdentityFile ~/.ssh/school'
-}
-
-file_line { 'ssh_config':
+file_line { 'Turn off passwd auth':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no'
+}
+
+file_line { 'Declare identity file':
+  path => '/etc/ssh/ssh_config',
+  line => 'IdentityFile ~/.ssh/school'
 }
