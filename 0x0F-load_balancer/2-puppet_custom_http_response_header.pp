833d92ff060cd7e174f757bf3ee70a5f10b66fb1
Usman Sharafa <Usmansharafa04@gmail.com>
2023-04-04 04:20:28 +0100

    Task 2 solved

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,29 +1,16 @@
 # Sets up a Nginx server with custom HTTP header
 
-exec { 'update':
-        command => '/usr/bin/apt-get update',
+file { '/etc/nginx/conf.d/custom-header.conf':
+  ensure  => file,
+  content => "add_header X-Served-By $hostname;",
+  mode    => '0644',
+  owner   => 'root',
+  group   => 'root',
+  notify  => Service['nginx'],
 }
 
-package { 'nginx':
-	ensure => 'installed',
-	require => Exec['update']
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
 
-file {'/var/www/html/index.html':
-	content => 'Hello World!'
-}
-
-exec {'redirect_me':
-	command => 'sed -i "24i\	rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;" /etc/nginx/sites-available/default',
-	provider => 'shell'
-}
-
-exec {'header':
-	command => 'sed -i "25i\	add_header X-Served-By \$HOSTNAME;" /etc/nginx/sites-available/default',
-	provider => 'shell'
-}
-
-service {'nginx':
-	ensure => running,
-	require => Package['nginx']
-}
