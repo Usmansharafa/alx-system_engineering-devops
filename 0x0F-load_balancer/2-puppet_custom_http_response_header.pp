91978964a1f6e6b6bc1f36066ab7eb6ada9ff510
Usman Sharafa <Usmansharafa04@gmail.com>
2023-04-04 03:59:20 +0100

    Task 2 solved

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,9 @@
+# Installs a Nginx server with custom HTTP header
+
+exec { 'custom header':
+  command  => 'apt-get -y update;
+  apt-get -y install nginx;
+  sudo sed -i "/listen 80 default_server;/a add_header X-Served-By $HOSTNAME;" /etc/nginx/sites-available/default;
+  service nginx restart',
+  provider => shell,
+}}
