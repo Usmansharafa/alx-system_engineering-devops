0987eb970b69daa9f373b4761198645461ae6c0c
Usman Sharafa <Usmansharafa04@gmail.com>
2023-04-06 17:32:45 +0100

    Task 2 solved

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,5 +1,20 @@
 # Sets up a Nginx server with custom HTTP header
 
+package {
+    'nginx':
+    ensure => installed,
+}
+
+file {'/var/www/html/index.nginx-debian.html':
+    content => 'Hello World!',
+}
+
+file_line {'configure redirection':
+    path  =>  '/etc/nginx/sites-available/default',
+    after =>  'server_name _;',
+    line  =>  "\n\tlocation /redirect_me {\n\t\treturn 301 https://youtube.com/watch?v=QH2-TGUlwu4;\n\t}\n",
+}
+
 file { '/etc/nginx/conf.d/custom-header.conf':
   ensure  => file,
   content => "add_header X-Served-By $hostname;",
@@ -13,4 +28,3 @@ service { 'nginx':
   ensure => running,
   enable => true,
 }
-
