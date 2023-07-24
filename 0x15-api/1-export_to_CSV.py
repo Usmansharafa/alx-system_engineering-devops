f6383499da46031c3404dc64ea9eae52b51b94ab
Usman Sharafa <Usmansharafa04@gmail.com>
2023-07-25 00:43:25 +0100

    Task 1 solved

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
     if len(argv) != 2:
         exit(1)
 
-    u_id = argv[1]
+    u_id = int(argv[1])
     api_url = "https://jsonplaceholder.typicode.com"
     url = f"{api_url}/users/{u_id}"
     users = requests.get(url).json()
@@ -23,11 +23,10 @@ if __name__ == "__main__":
         if key["userId"] == u_id:
             tasks_for_id.append(key)
 
-    u_name = users["username"]
     file_name = f"{u_id}.csv"
 
     with open(file_name, "w", newline="") as file:
         writer = csv.writer(file, quoting=csv.QUOTE_NONNUMERIC)
         for key in tasks_for_id:
             writer.writerow([str(u_id), users["username"],
-                str(key["completed"]), key["title"]])
+                            str(key["completed"]), key["title"]])
